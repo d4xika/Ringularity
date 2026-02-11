2da146a209112d8c5e744b79d208460edd86d015
Marie <mariehofer64@gmail.com>
2026-02-11 13:50:43 +0100

    style: changed dialogs into sheets, bigbutton color adaptable

--- ringularity/lib/widgets/common/big_button.dart
+++ ringularity/lib/widgets/common/big_button.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+
 import '../../theme/app_colors.dart';
 
 class BigButton extends StatelessWidget {
   final Widget child;
   final VoidCallback onPressed;
+  final Color? backgroundColor;
 
-  const BigButton({super.key, required this.child, required this.onPressed});
+  const BigButton({
+    super.key,
+    required this.child,
+    required this.onPressed,
+    this.backgroundColor,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +22,7 @@ class BigButton extends StatelessWidget {
       child: ElevatedButton(
         onPressed: onPressed,
         style: ElevatedButton.styleFrom(
-          backgroundColor: AppColors.cardBackground,
+          backgroundColor: backgroundColor ?? AppColors.cardBackground,
           surfaceTintColor: Colors.transparent,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(16),
