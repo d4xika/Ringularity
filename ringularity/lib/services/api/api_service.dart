5be08375a42657ce4d5d24972e61e0b782d3d6f7
Katja Daxberger <katja.daxi@gmail.com>
2026-02-18 17:10:35 +0100

    fix: upsi

--- ringularity/lib/services/api/api_service.dart
+++ ringularity/lib/services/api/api_service.dart
@@ -8,8 +8,7 @@ import 'package:ringularity/services/secure_storage_service.dart';
 import '../../models/activity_model.dart';
 
 class ApiService extends ChangeNotifier {
-  //static const String _baseUrl = 'http://10.25.6.11:3000/api';
-  static const String _baseUrl = 'http://192.168.178.86:3000/api';
+  static const String _baseUrl = 'http://10.25.6.11:3000/api';
 
   //TODO: add button to sync data to the backend
   //and back to phone
